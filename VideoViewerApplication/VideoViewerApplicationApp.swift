d7957462a3d666f52bde3cbbf021dd9321744a2a
timberra <liga.griezne@gmail.com>
2024-04-29 09:31:02 +0300

    Initial Commit

---
+++ VideoViewerApplication/VideoViewerApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoViewerApplicationApp.swift
+//  VideoViewerApplication
+//
+//  Created by liga.griezne on 29/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct VideoViewerApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
