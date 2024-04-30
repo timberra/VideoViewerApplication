6687fc9f1d1a8735d96ce133ea16cc43ffabc25a
timberra <liga.griezne@gmail.com>
2024-04-30 12:59:14 +0300

    Added custom alert for download

--- VideoViewerApplication/View/VideoDetailView.swift
+++ VideoViewerApplication/View/VideoDetailView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 import AVKit
 
 struct VideoDetailView: View {
+    @State private var showAlert = false
     @StateObject var videoDownloadManager = VideoDownloadManager()
+    @State private var isDownloading = false
     let video: Video
     
     var body: some View {
@@ -25,26 +27,65 @@ struct VideoDetailView: View {
         .toolbar {
             ToolbarItem(placement: .navigationBarTrailing) {
                 if videoDownloadManager.downloadProgress > 0 {
-                    ProgressBar(progress: videoDownloadManager.downloadProgress)
+                    ProgressView(value: videoDownloadManager.downloadProgress, total: 1)
+                        .progressViewStyle(LinearProgressViewStyle())
+                        .padding(.trailing)
                     Button("Cancel Download") {
                         videoDownloadManager.cancelDownload()
+                        isDownloading = false // Set isDownloading to false
                     }
+                    .padding(.trailing)
                 } else {
                     Button("Download Video") {
+                        isDownloading = true
                         videoDownloadManager.startDownload(video: video)
+                        showAlert = true
                     }
+                    .padding(.trailing)
                 }
             }
         }
+        .sheet(isPresented: $showAlert) {
+            CustomAlertView(downloadProgress: $videoDownloadManager.downloadProgress, onCancel: {
+                showAlert = false
+            })
+            .background(Color(UIColor.systemBackground))
+        }
     }
 }
-
-struct ProgressBar: View {
-    var progress: Float
-    
-    var body: some View {
-        ProgressView(value: progress, total: 1)
-            .progressViewStyle(LinearProgressViewStyle())
-            .padding()
+//MARK: - CustomAlertView
+extension VideoDetailView {
+    struct CustomAlertView: View {
+        @Binding var downloadProgress: Float
+        var onCancel: () -> Void
+        var body: some View {
+            GeometryReader { geometry in
+                ZStack {
+                    Color.clear
+                        .opacity(0.001)
+                        .frame(width: geometry.size.width, height: geometry.size.height)
+                        .onTapGesture {}
+                    VStack {
+                        Text("Downloading...")
+                            .font(.headline)
+                            .foregroundColor(Color(UIColor.label))
+                            .padding()
+                        Text("Download progress: \(Int(downloadProgress * 100))%")
+                            .padding()
+                            .foregroundColor(Color(UIColor.label))
+                        Button("Cancel") {
+                            onCancel()
+                        }
+                        .padding()
+                        .foregroundColor(Color.blue)
+                    }
+                    .frame(width: geometry.size.width * 0.8, height: geometry.size.height * 0.3)
+                    .background(Color(UIColor.secondarySystemBackground))
+                    .cornerRadius(10)
+                    .shadow(radius: 5)
+                }
+            }
+            .ignoresSafeArea()
+        }
     }
 }
