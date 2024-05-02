df5ba9ff7dda474fe87a7374d49b01c6ec92e6e6
timberra <liga.griezne@gmail.com>
2024-05-02 11:54:53 +0300

    Added Test

--- VideoViewerApplicationTests/VideoViewerApplicationTests.swift
+++ VideoViewerApplicationTests/VideoViewerApplicationTests.swift
@@ -8,29 +8,72 @@
 import XCTest
 @testable import VideoViewerApplication
 
-final class VideoViewerApplicationTests: XCTestCase {
-
+class VideoDownloadManagerTests: XCTestCase {
+    
+    var videoDownloadManager: VideoDownloadManager!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        videoDownloadManager = VideoDownloadManager()
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        videoDownloadManager = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testStartDownload() throws {
+        let videoURL = URL(string: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4")!
+        let thumbnailURL = URL(string: "https://static.vecteezy.com/system/resources/thumbnails/005/939/000/small/simple-test-icon-editable-48-pixel-free-vector.jpg")!
+        let video = Video(id: 1, name: "Test Video", description: "Test Description", thumbnail: thumbnailURL, videoURL: videoURL)
+        
+        let progressExpectation = expectation(description: "Download progress should increase")
+        
+        // Start the download
+        videoDownloadManager.startDownload(video: video)
+        
+        // Wait for a brief moment to allow the progress to update
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            // Assert that the download progress has increased
+            XCTAssertGreaterThan(self.videoDownloadManager.downloadProgress, 0)
+            
+            // Fulfill the expectation
+            progressExpectation.fulfill()
+        }
+        
+        // Wait for the expectation to be fulfilled or timeout after a certain duration
+        waitForExpectations(timeout: 2.0, handler: nil)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testCancelDownload() throws {
+        let videoURL = URL(string: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4")!
+        let thumbnailURL = URL(string: "https://static.vecteezy.com/system/resources/thumbnails/005/939/000/small/simple-test-icon-editable-48-pixel-free-vector.jpg")!
+        let video = Video(id: 1, name: "Test Video", description: "Test Description", thumbnail: thumbnailURL, videoURL: videoURL)
+        
+        videoDownloadManager.startDownload(video: video)
+        videoDownloadManager.cancelDownload()
+        
+        // Assert that the download progress is 0 after canceling
+        XCTAssertEqual(videoDownloadManager.downloadProgress, 0)
+    }
+    
+    func testDownloadProgress() throws {
+        // Ensure videoDownloadManager is initialized
+        XCTAssertNotNil(videoDownloadManager, "VideoDownloadManager should not be nil")
+        
+        guard let videoDownloadManager = videoDownloadManager else {
+            XCTFail("videoDownloadManager is nil")
+            return
+        }
+        
+        let thumbnailURL = URL(string: "https://static.vecteezy.com/system/resources/thumbnails/005/939/000/small/simple-test-icon-editable-48-pixel-free-vector.jpg")!
+        let videoURL = URL(string: "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4")!
+        let video = Video(id: 1, name: "Test Video", description: "Test Description", thumbnail: thumbnailURL, videoURL: videoURL)
+        videoDownloadManager.startDownload(video: video)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            // Assert that the download progress has increased
+            XCTAssertGreaterThan(videoDownloadManager.downloadProgress, 0)
         }
     }
+
 
 }
